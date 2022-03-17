defe826516f1ea3b5a84e5edc6fb4172f06e7a1b
Amin Meshk <amin.meshk@gmail.com>
2022-03-17 19:58:38 +0330

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,60 @@
+import { useCallback, useEffect, useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [foxUrl, setFoxUrl] = useState('');
+  const fetchFoxUrl = useCallback(async () => {
+    try {
+      const res = await fetch('https://randomfox.ca/floof/');
+      const imgUrl = (await res.json()).image;
+      console.log(imgUrl);
+      setFoxUrl(imgUrl as string);
+    } catch (error) {
+      console.log('error', error);
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchFoxUrl();
+  }, [fetchFoxUrl]);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={foxUrl} className="App-logo" alt="logo" />
+        <p>Hello Vite + React!</p>
+        <p>
+          <button type="button" onClick={() => setCount((count) => count + 1)}>
+            count is: {count}
+          </button>
+        </p>
+        <p>
+          Edit <code>App.tsx</code> and save to test HMR updates.
+        </p>
+        <p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          {' | '}
+          <a
+            className="App-link"
+            href="https://vitejs.dev/guide/features.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Vite Docs
+          </a>
+        </p>
+      </header>
+    </div>
+  )
+}
+
+export default App
